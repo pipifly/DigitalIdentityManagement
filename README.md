2a90e7b4742be367c0206634393453b4ab9fb7c0
j5465 <41243685+j5465@users.noreply.github.com>
2022-04-14 15:11:30 +0800

    公钥到地址，验签方法

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+### ERC728 DID claim holder例子
 
 可以先看
 [**erc725 使用例子**](
@@ -5,21 +6,22 @@ https://hackernoon.com/first-impressions-with-erc-725-and-erc-735-identity-and-c
 里面有DID持有，发行，验证的例子
 然后 main.js 是与这些合约交互的步骤。
 
+----
+
+### js给数据签名和验签
+
 signData.js 是看[**web3, secp256k1 签名与Solidity验签**](https://zhuanlan.zhihu.com/p/69542679)写的。
 里面有给数据用secp256k1签名，可以用solidity的keccak256 验签。
 
 [**secp256k1包API**](https://github.com/cryptocoinjs/secp256k1-node/blob/v3.x/API.md)
 
-signString 函数里给数据签名后，不知道怎么用keccak256验签
-recover 后的公钥是 0x02caa5418a17c694e5ec4d898f60f5b5f75be6586a7cefb8c53ae581dc3bc0f821
+signString 函数里给数据签名后，recover 后的公钥是 *04caa5418a17c694e5ec4d898f60f5b5f75be6586a7cefb8c53ae581dc3bc0f821febe79f60a48722b0fd7fdff9585dd2f4afbe2434df6faad4d0a25da992c6140*
 
-但公钥应该是 0x73d6189c34C0A357A5850f315eE2120be1F2E5cd
-```
-  secp256k1.verify(buf_sha_hash, sig.signature, buf_pubKey);
-  const recover = secp256k1.recover(buf_sha_hash, sig.signature, 0).toString("hex");
-  console.log(recover, recover.length); 
-```
+该私钥对应的以太坊地址是 0x73d6189c34C0A357A5850f315eE2120be1F2E5cd
 
+[**这篇文章介绍了以太坊私钥怎么生成公钥，公钥再生成地址**](https://zhuanlan.zhihu.com/p/149821832)
+
+signdata演示了给数据签名，验签，和公钥生成以太坊地址的过程。
 
 ```
 npm install 
--- src/signData.js
+++ src/signData.js
@@ -1,12 +1,11 @@
 import Web3 from 'web3'
 import secp256k1 from 'secp256k1';
+import createKeccakHash from 'keccak';
 
 const privateKey = '0xa641dd97b3e72a419e483b99d61998b3ade7fd8f50296e0df382af74334a0131';
-const publicKey = '0x73d6189c34C0A357A5850f315eE2120be1F2E5cd';
+const address = '0x73d6189c34C0A357A5850f315eE2120be1F2E5cd';
 
-
-
-function createAccount(web3) {
+function createAccount(Web3) {
   // {
   //   address: '0x73d6189c34C0A357A5850f315eE2120be1F2E5cd',
   //   privateKey: '0xa641dd97b3e72a419e483b99d61998b3ade7fd8f50296e0df382af74334a0131',
@@ -14,13 +13,23 @@ function createAccount(web3) {
   //   sign: [Function: sign],
   //   encrypt: [Function: encrypt]
   // }
-  return web3.eth.accounts.create();
+  return Web3.eth.accounts.create();
+}
+
+function signEthTxn () {
+  const orderHash = Web3.utils.asciiToHex("fk!!!");
+  console.log("直接使用web3.eth.accounts.sign, 会自动带上preifix。签名结果: ", Web3.eth.accounts.sign(orderHash, privateKey));
+}
+
+function recoverPublicKey(data, signature) {
+  const recover = secp256k1.recover(data, signature, 0, false).toString("hex");
+  return recover;
 }
 
 function signString(string, privateKey) {
 
   const dataHex = Web3.utils.utf8ToHex(string);
-  console.log(dataHex);
+  
   const sha_hash = Web3.utils.soliditySha3(dataHex); // 转成32bytes长
   const bytes32_sha_hash = Web3.utils.hexToBytes(sha_hash);
   
@@ -28,43 +37,62 @@ function signString(string, privateKey) {
   const buf_privateKey = Buffer.from(Web3.utils.hexToBytes(privateKey));
 
   const sig = secp256k1.sign(buf_sha_hash, buf_privateKey);
-  console.log(sig);
   const ret = {};
   ret.r = "0x" + sig.signature.slice(0, 32).toString('hex');
   ret.s = "0x" + sig.signature.slice(32, 64).toString('hex');
   ret.v = "0x" + (sig.recovery + 27).toString(16);
-  console.log(sig.signature);
-  // const buf_sig = Buffer.from(Web3.utils.hexToBytes(sig.signature));
-  const  buf_pubKey = Buffer.from(Web3.utils.hexToBytes(publicKey));
+  
+  const recoveredPublicKey = recoverPublicKey(buf_sha_hash, sig.signature);
+  console.log("recovered pub key: ", recoveredPublicKey);
 
-  // secp256k1.verify(buf_sha_hash, sig.signature, buf_pubKey);
-  // const recover = secp256k1.recover(buf_sha_hash, sig.signature, 0).toString("hex");
-  // console.log(recover, recover.length);
+  const publicKey = createPublicKey(privateKey);
+  console.log("public key from privatekey: ", publicKey);
+
+  console.log("recovered == public key", recoveredPublicKey === publicKey);
+  
+  const addressFromPublicKey = publicKeyToAddress(publicKey);
+
+  console.log("address from public key: ", addressFromPublicKey)
+  console.log("address: ", address);
+
   return ret;
 }
 
-function secp (web3) {
-  const orderHash = web3.utils.asciiToHex("fk!!!");
-  // const orderHash = "0xa641dd97b3e72a419e483b99d61998b3ade7fd8f50296e0df382af74334a0131";
-  console.log("直接使用web3.eth.accounts.sign, 会自动带上preifix。签名结果: ", web3.eth.accounts.sign(orderHash, privateKey));
+
+function createPublicKey(privateKey) {
+  const uint8_prikey = Uint8Array.from(Buffer.from(Web3.utils.hexToBytes(privateKey)));
+  const pubkey = secp256k1.publicKeyCreate(uint8_prikey, false).toString('hex');
+  return pubkey;
 }
 
 
+function publicKeyToAddress (publicKey) {
+  if (!Buffer.isBuffer(publicKey)) {
+    if (typeof publicKey !== 'string') {
+      throw new Error('Expected Buffer or string as argument')
+    }
+
+    publicKey = publicKey.slice(0, 2) === '0x' ? publicKey.slice(2) : publicKey
+    publicKey = Buffer.from(publicKey, 'hex')
+  }
+  
+  publicKey = Buffer.from(secp256k1.publicKeyConvert(publicKey, false)).slice(1)
+  const hash = createKeccakHash('keccak256').update(publicKey).digest()
+  return Web3.utils.toChecksumAddress(hash.slice(-20).toString('hex'))
+}
+
 (async () => {
   // const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/4fd2305497f349fb81fba70405c9f9fe"))
-  const web3 = await new Web3();
-  web3.eth.accounts.privateKeyToAccount('0xa641dd97b3e72a419e483b99d61998b3ade7fd8f50296e0df382af74334a0131');
-  // console.log(createAccount(web3));
+  // const web3 = await new Web3();
+  // web3.eth.accounts.privateKeyToAccount('0xa641dd97b3e72a419e483b99d61998b3ade7fd8f50296e0df382af74334a0131');
   // const accounts = await web3.eth.personal.getAccounts();
   // console.log(await web3.eth.getAccounts())
-  
-  // secp(web3);
+
   const dataString = JSON.stringify({
     "name": "张三",
     "stuId": 124346356,
     "major": "cs"
   });
   console.log(signString(dataString, privateKey));
-
-
+  // const address = publicKeyToAddress(createPubkey(privateKey));
 })()
